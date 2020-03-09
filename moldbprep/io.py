52c0468c1c4238cca4beeabbd29199b6c4b2ca0c
David Schaller <schaller.david@yahoo.de>
2020-03-09 11:19:39 +0100

    added sdf_properties function

--- moldbprep/io.py
+++ moldbprep/io.py
@@ -6,7 +6,7 @@ def count_sdf_mols(file_path):
 
     Parameters
     ----------
-    file_path :  str
+    file_path : str
         Full path to sdf file.
 
     Returns
@@ -21,3 +21,28 @@ def count_sdf_mols(file_path):
             if '$$$$' in line:
                 counter += 1
     return counter
+
+
+def sdf_properties(file_path):
+    """
+    This function returns a list of properties stored in an sdf-file.
+
+    Parameters
+    ----------
+    file_path : str
+         Full path to sdf file.
+
+    Returns
+    -------
+    properties : list
+        Properties stored in sdf-file.
+
+    """
+    properties = []
+    with open(file_path, 'r') as sdf_file:
+        for line in sdf_file:
+            if '>  <' in line:
+                properties.append(line.strip()[4:-1])
+            elif '$$$$' in line:
+                break
+    return properties
