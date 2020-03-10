48aa963fc88090b61acb7916e12712297ddb92f6
David Schaller <schaller.david@yahoo.de>
2020-03-10 08:31:46 +0100

    fix path in test_io.py

--- moldbprep/tests/test_io.py
+++ moldbprep/tests/test_io.py
@@ -3,7 +3,7 @@ import os
 
 
 def test_count_sdf_mols():
-    assert count_sdf_mols(os.path.join(os.getcwd(), os.pardir, "data", "sample.sdf")) == 3
+    assert count_sdf_mols(os.path.join(os.getcwd(), "moldbprep", "data", "sample.sdf")) == 3
 
 def test_sdf_properties():
-    assert sdf_properties(os.path.join(os.getcwd(), os.pardir, "data", "sample.sdf")) == ['ID', 'vendor']
+    assert sdf_properties(os.path.join(os.getcwd(), "moldbprep", "data", "sample.sdf")) == ['ID', 'vendor']
