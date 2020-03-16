b3f077f9288f58b477e41de3e2472b5c07d792ad
David Schaller <schaller.david@yahoo.de>
2020-03-16 17:34:03 +0100

    new protonation tests

--- moldbprep/tests/test_standardize.py
+++ moldbprep/tests/test_standardize.py
@@ -22,7 +22,31 @@ def test_largest_fragment(input_smiles, output_smiles):
     ("CCP(O)(O)=O", "CCP([O-])([O-])=O"),
     ("CS(=O)(=O)NC=O", "CS(=O)(=O)[N-]C=O"),
     ("CC1=NNN=N1", "CC1=N[N-]N=N1"),
-    ("CC(=O)C=CO", "CC(=O)C=C[O-]")
+    ("CC(=O)C=CO", "CC(=O)C=C[O-]"),
+    ("c1(C2CCCCC2)[nH]nnn1", "c1(C2CCCCC2)nnn[n-]1"),
+    ("c1(C2CCCCC2)n[nH]nn1", "c1(C2CCCCC2)nn[n-]n1"),
+    ("c1(C2CCCCC2)nnn[n-]1", "c1(C2CCCCC2)nnn[n-]1"),
+    ("c1(C2CCCCC2)nn[n-]n1", "c1(C2CCCCC2)nn[n-]n1"),
+    ("O=C1C(O)=C(O)C([C@H](O)CO)O1", "O=C1C(O)=C([O-])C([C@H](O)CO)O1"),
+    ("O=C1C(O)=C([O-])C([C@H](O)CO)O1", "O=C1C(O)=C([O-])C([C@H](O)CO)O1"),
+    ("O=C(O)C1CCCCC1", "O=C([O-])C1CCCCC1"),
+    ("O=C(N)C=1C(=O)[C@@]2(O)C(O)=C3C(=O)c4c(O)cccc4[C@H](C)[C@H]3[C@H](O)[C@@H]2[C@H](N(C)C)C=1O",
+     "O=C(N)C=1C(=O)[C@@]2(O)C(O)=C3C(=O)c4c(O)cccc4[C@H](C)[C@H]3[C@H](O)[C@@H]2[C@H]([N+H](C)C)C=1[O-]"),
+    ("S(=O)(=O)(NC(=O)NCCCC)c1ccc(C)cc1", "S(=O)(=O)([N-]C(=O)NCCCC)c1ccc(C)cc1"),
+    ("S(=O)(=O)(Nc1noc(C)c1)c1ccc(N)cc1", "S(=O)(=O)(Nc1noc(C)c1)c1ccc(N)cc1"),
+    ("P(=O)(O)(O)[C@@H]1[C@H](C)O1", "P(=O)([O-])([O-])[C@@H]1[C@H](C)O1"),
+    ("S(=O)(=O)(O)c1ccc(C)cc1", "S(=O)(=O)([O-])c1ccc(C)cc1"),
+    ("O=C(Oc1cc2[C@@]3(C)[C@@H](N(C)c2cc1)N(C)CC3)NC", "O=C(Oc1cc2[C@@]3(C)[C@H]([N+H](C)CC3)N(C)c2cc1)NC"),
+    ("S(=O)(=O)(NC(=N)N)c1ccc(N)cc1", "S(=O)(=O)(NC(=N)N)c1ccc(N)cc1"),
+    ("S(CCN/C(=N/C#N)/NC)Cc1c(C)nc[nH]1", "S(CCN/C(=N/C#N)/NC)Cc1c(C)nc[nH]1"),
+    ("Clc1cc2nccc(N[C@H](CCCN(CC)CC)C)c2cc1", "Clc1cc2nccc(N[C@H](CCC[N+H](CC)CC)C)c2cc1"),
+    ("O=C(O)[C@@H](N)Cc1nc[nH]c1", "O=C([O-])[C@@H]([N+H3])Cc1nc[nH]c1"),
+    ("FC1(F)C(N)CCCC1", "FC1(F)C(N)CCCC1"),
+    ("[C@H](CN1C[C@@H](C)O[C@@H](C)C1)(Cc1ccc(C(CC)(C)C)cc1)C",
+     "[C@H](CN1C[C@@H](C)O[C@@H](C)C1)(Cc1ccc(C(CC)(C)C)cc1)C"),
+    ("O[C@@H](CNC)c1cc(O)c(O)cc1", "O[C@@H](C[N+H2]C)c1cc(O)c(O)cc1"),
+    ("O(C)c1c(OC)cc(Cc2c(N)nc(N)nc2)cc1OC", "O(C)c1c(OC)cc(Cc2c(N)nc(N)nc2)cc1OC"),
+    ("O=C(NN)c1ccncc1", "O=C(NN)c1ccncc1")
 ])
 def test_protonate_mol(input_smiles, output_smiles):
     assert Chem.MolToSmiles(protonate_mol(Chem.MolFromSmiles(input_smiles))) == \
