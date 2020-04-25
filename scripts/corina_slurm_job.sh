9fb00c7dde090dd9441c3c847a4df943901afeaf
David Schaller <schaller.david@yahoo.de>
2020-04-25 20:59:12 +0200

    fix corina path3

--- scripts/corina_slurm_job.sh
+++ scripts/corina_slurm_job.sh
@@ -4,4 +4,4 @@ path="$1"
 msi="$2"
 msc="$3"
 
-echo "/software/bin/corina -d stergen,msi=$msi,msc=$msc,preserve,wh,r2d -i t=sdf $path -o t=sdf ${path:0:-4}_3D.sdf"
+/software/bin/corina -d stergen,msi=$msi,msc=$msc,preserve,wh,r2d -i t=sdf $path -o t=sdf ${path:0:-4}_3D.sdf
